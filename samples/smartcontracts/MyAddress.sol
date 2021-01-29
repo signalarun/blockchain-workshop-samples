68409f986b5869be463ff0b0a4bfa2e3d8e9143e
Arun Gopi <signalarun@gmail.com>
2021-01-29 14:07:35 +0530

    Create MyAddress.sol

---
+++ samples/smartcontracts/MyAddress.sol
@@ -0,0 +1,21 @@
+// pragma : Version information
+
+pragma solidity ^0.6.0;
+
+// importing source file
+
+// import path/of/file;
+
+
+// contract
+contract MyAddress{
+    // Logic
+    
+    address public myAddress;
+    
+    function setAddress(address val) public{
+        myAddress = val;
+        
+    }
+}
+
