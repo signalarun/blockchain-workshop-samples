a91f9aef302e4291aba8b55a10c89065911ebc51
Arun Gopi <signalarun@gmail.com>
2021-01-29 17:33:25 +0530

    Create MyBalanceTransferApp.sol

---
+++ samples/smartcontracts/MyBalanceTransferApp.sol
@@ -0,0 +1,24 @@
+// pragma : Version information
+
+pragma solidity ^0.6.0;
+
+// importing source file
+
+// import path/of/file;
+
+
+// contract
+contract MyBalanceTransferApp{
+    // Logic
+    
+    uint public myTotalBalance;
+    
+    function receiveBalance() public payable{
+      myTotalBalance += msg.value;
+    }
+    
+    
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+}
