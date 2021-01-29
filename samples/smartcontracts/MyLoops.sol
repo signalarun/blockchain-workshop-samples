739df8b107a2012031ee5cfd1dcea92740830e59
Arun Gopi <signalarun@gmail.com>
2021-01-29 14:50:46 +0530

    Create MyLoops.sol

---
+++ samples/smartcontracts/MyLoops.sol
@@ -0,0 +1,31 @@
+// pragma : Version information
+
+pragma solidity ^0.6.0;
+
+// importing source file
+
+// import path/of/file;
+
+
+// contract
+contract MyLoops{
+    uint public myVal;
+    
+    function performLooping() public {
+        
+        
+        uint n;
+    
+        // While loop
+        
+        while(n < 10 /*condition*/){
+            
+            myVal++;
+            n++;
+            
+        }
+        
+        
+        
+    }
+}
