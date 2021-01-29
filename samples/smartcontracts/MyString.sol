af31fc7f92328d9ec410a045f44ba5711dcb81e5
Arun Gopi <signalarun@gmail.com>
2021-01-29 14:15:50 +0530

    Create MyString.sol

---
+++ samples/smartcontracts/MyString.sol
@@ -0,0 +1,21 @@
+// pragma : Version information
+
+pragma solidity ^0.6.0;
+
+// importing source file
+
+// import path/of/file;
+
+
+// contract
+contract MyString{
+    // Logic
+    
+    string public myString;
+    
+    function setString(string memory val) public{
+        myString = val;
+        
+    }
+}
+
