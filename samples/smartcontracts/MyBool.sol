167a490f8f75bec3486c56b01bcb6e605ebd76ba
Arun Gopi <signalarun@gmail.com>
2021-01-29 12:39:40 +0530

    Update MyBool.sol

--- samples/smartcontracts/MyBool.sol
+++ samples/smartcontracts/MyBool.sol
@@ -1,1 +1,17 @@
 
+// pragma : Version information
+
+pragma solidity ^0.6.0;
+
+// importing source file
+
+// import path/of/file;
+
+
+// contract
+contract MyBool{
+    bool public myFlag;
+    function setFlag() public{
+        myFlag = true;
+    }
+}
